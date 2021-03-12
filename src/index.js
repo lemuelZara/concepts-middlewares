02eeaa8825575cef7f845f36453bab86da0ec4a6
Lemuel <lemuel.czara@gmail.com>
2021-03-12 14:27:39 -0300

    feat: create middleware to checks todo exists

--- src/index.js
+++ src/index.js
@@ -37,7 +37,28 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: '' });
+  }
+
+  const idIsValid = validate(id); 
+  if (!idIsValid) {
+    return response.status(400).json({ error: '' });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: '' });
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
